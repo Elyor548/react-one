e414f8439fdf84588e8b9d67255c2caa480181d4
Elyor <mansurovelyor08@gmail.com>
2025-05-22 23:26:41 +0500

    дачали делать компоненты

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className="header">
+    <img src = 'https://images.icon-icons.com/1121/PNG/512/1486147222-social-media-network22_79488.png' />
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className="content">
+    <div>
+    <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
+    </div>
+    <div>
+      
+    </div>
+
+    <div>
+      my phot
+      <div>
+        post
+      </div>
+    </div>
+  </div>
+    
+
+}
+
+export default Profile;

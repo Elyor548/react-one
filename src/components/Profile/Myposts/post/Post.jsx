9d9b5e5f5c79878341ec2d62e9dea14ff9c5fd72
Elyor <mansurovelyor08@gmail.com>
2025-05-23 18:02:10 +0500

    создали компоненты

---
+++ src/components/Profile/Myposts/post/Post.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import s from './Post.module.css'
+
+const Post = () => {
+    return  (
+        <div className={s.item}>
+          <img src="https://3dnews.ru/assets/external/illustrations/2025/01/07/1116364/0.jpg" />
+          post 1
+          <div>
+          <span>lile</span>
+          </div>
+        </div>
+    )
+} 
+
+export default Post;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from './Profile.module.css'
+import MyPosts from "./Myposts/MyPosts";
 
 const Profile = () => {
     return <div className={s.content}>
@@ -7,23 +8,9 @@ const Profile = () => {
     <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
     </div>
     <div>
-      
+      ava + description
     </div>
-
-    <div>
-      my phot
-      <div>
-        New post
-      </div>
-      <div className={s.posts}>
-        <div className={s.item}>
-          post 1
-        </div>
-        <div className={s.item}>
-          post 2 
-        </div>
-      </div>
-    </div>
+    <MyPosts />
   </div>
     
 

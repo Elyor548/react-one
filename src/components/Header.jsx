1a0c3e7f254864c747d423e0b4306c9565fe04e8
Elyor <mansurovelyor08@gmail.com>
2025-05-23 15:45:56 +0500

    сделали css module

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import s from './Header.module.css'
 
 const Header = () => {
-    return <header className="header">
+    return <header className={s.header}>
     <img src = 'https://images.icon-icons.com/1121/PNG/512/1486147222-social-media-network22_79488.png' />
   </header>
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import s from './Profile.module.css'
 
 const Profile = () => {
-    return <div className="content">
+    return <div className={s.content}>
     <div>
     <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg' />
     </div>
@@ -12,7 +13,15 @@ const Profile = () => {
     <div>
       my phot
       <div>
-        post
+        New post
+      </div>
+      <div className={s.posts}>
+        <div className={s.item}>
+          post 1
+        </div>
+        <div className={s.item}>
+          post 2 
+        </div>
       </div>
     </div>
   </div>
